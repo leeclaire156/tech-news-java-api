e7a83c74fe1d032c47a99af57c11fb3838d3c439
Claire Lee <lee.claire156@gmail.com>
2023-06-13 22:52:22 -0400

    Adds Comment controllers

--- src/main/java/com/technews/controller/PostController.java
+++ src/main/java/com/technews/controller/PostController.java
@@ -9,7 +9,7 @@ import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
+import javax.serv
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
