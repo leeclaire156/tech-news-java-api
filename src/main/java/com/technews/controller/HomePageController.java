39eb9b74ff9140ed6d44d30b44c4499f42f57d63
Claire Lee <lee.claire156@gmail.com>
2023-06-28 21:54:46 -0400

    Adds homepage endpoint

--- src/main/java/com/technews/controller/HomePageController.java
+++ src/main/java/com/technews/controller/HomePageController.java
@@ -1,5 +1,7 @@
 package com.technews.controller;
 
+import com.technews.model.Post;
+import com.technews.model.User;
 import com.technews.repository.CommentRepository;
 import com.technews.repository.PostRepository;
 import com.technews.repository.UserRepository;
@@ -7,6 +9,8 @@ import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomePageController {
@@ -41,4 +45,32 @@ public class HomePageController {
         return "redirect:/login";
     }
 
+    @GetMapping("/")
+    public String homepageSetup(Model model, HttpServletRequest request) {
+        User sessionUser = new User();
+
+        if (request.getSession(false) != null) {
+            sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+            model.addAttribute("loggedIn", sessionUser.isLoggedIn());
+        } else {
+            model.addAttribute("loggedIn", false);
+        }
+
+
+        List<Post> postList = postRepository.findAll();
+        for (Post p : postList) {
+            p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
+            User user = userRepository.getById(p.getUserId());
+            p.setUserName(user.getUsername());
+        }
+
+        model.addAttribute("postList", postList);
+        model.addAttribute("loggedIn", sessionUser.isLoggedIn());
+
+        // "point" and "points" attributes refer to upvotes.
+        model.addAttribute("point", "point");
+        model.addAttribute("points", "points");
+
+        return "homepage";
+    }
 }
